13d9c4e8b93a20d9575c5b4f8b742bc26d9cbe96
Lars Yencken <lars@yencken.org>
2015-04-12 15:40:41 +1000

    Detect and correct bad mono samples.

--- src/audio.py
+++ src/audio.py
@@ -11,6 +11,7 @@ Audio manipulation utilities.
 import contextlib
 import tempfile
 
+import numpy as np
 import pydub
 import sh
 
@@ -22,6 +23,9 @@ def cropped(mp3_file, offset, duration, adjust_volume=True):
     whole_clip = pydub.AudioSegment.from_mp3(mp3_file)
     selected = whole_clip[offset * MS_PER_S:(offset + duration) * MS_PER_S]
 
+    if is_bad_mono(selected):
+        selected = selected.set_channels(1)
+
     with tempfile.NamedTemporaryFile(suffix='.mp3') as t:
         selected.export(t, format='mp3')
 
@@ -30,6 +34,25 @@ def cropped(mp3_file, offset, duration, adjust_volume=True):
             sh.mp3gain('-r', '-k', '-t', '-s', 'r', t.name)
 
         yield t.name
+
+
+def is_bad_mono(segment):
+    "Is there sound on only one channel?"
+    if segment.channels != 2:
+        return False
+
+    w = segment.sample_width
+    if w == 1:
+        dtype = np.uint8
+    elif w == 2:
+        dtype = np.uint16
+    else:
+        raise Exception("non-standard sample width: {0}".format(w))
+
+    a = np.fromstring(segment._data, dtype=dtype).reshape(
+        (int(segment.frame_count()), segment.channels)
+    )
+    return (a[:, 0] == 0).mean() > 0.4 or (a[:, 1] == 0).mean() > 0.4
 
 
 def play_mp3(mp3_file):
